e3849c3a2f120de2044bf152abce44458079f468
Serban <128298814+Serban096@users.noreply.github.com>
2024-02-04 04:56:03 +0200

    added users controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proiect.Services.UserService;
+
+namespace Proiect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserByUserName([FromBody] string username)
+        {
+            return Ok(_userService.GetUserByUsername(username));
+        }
+    }
+}
